0cf6eebb31363b6f7aaeb8144faa4a3291daee43
Liz Parody <lizparody@Lizs-MacBook-Pro.local>
2021-03-31 15:08:20 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e2bb8514c5a3ea06ee25d5d5bade433b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6b1b0a29-3dba-452a-9556-e40d22a3af74'
+};
